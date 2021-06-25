fb7731cd8fffec3567639e7ca5572fe93b54da79
yogesh-kansal <yogeshiitbhub@gmail.com>
2021-06-26 01:31:40 +0530

    modified utils

--- backend/utils/appError.js
+++ backend/utils/appError.js
@@ -1,7 +1,6 @@
 class appError extends Error {
     constructor(message, statusCode) {
         super(message);
-        this.err=message;
         this.status = statusCode; 
        // console.log(message);   
     }  
--- backend/utils/catchAsync.js
+++ backend/utils/catchAsync.js
@@ -2,5 +2,4 @@ module.exports = fcn => (req,res,next) => {
     return fcn(req,res,next).catch(err => next(err));
 } 
 
-//we are doing currying of function because first we are calling function
-//then after some time we are passing parameters
+//we are doing currying of function 
