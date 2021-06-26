3c9e7791462d4e0f3a9386513a8137f5f97c47bc
yogesh-kansal <yogeshiitbhub@gmail.com>
2021-06-26 19:21:22 +0530

    modified profile

--- frontend/src/components/User/edit.js
+++ frontend/src/components/User/edit.js
@@ -1,27 +1,55 @@
 import React, { Component } from 'react';
-import {Card, CardBody, CardHeader, Button, 
-    Form, FormGroup, Label, Input} from 'reactstrap';
-import './style.css';
+import {Input} from 'reactstrap';
+import { Link } from 'react-router-dom';
+import {authContext} from '../../contexts/authContext';
+import './style.css'
+import axios from 'axios';
+import {baseUrl} from '../../assets/config';
+import LoadingSpinner from '../loadingSpinner';
 
 class Edit extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            new_name: '',
-            new_email: '',
-            new_adds: '',
-            old_pass: '',
-            new_pass: '',
-            touched: {
-                user: '',
-                email: '',
-                add: '',
-            }
-        }
-        
+    static contextType=authContext;
+    state = {
+        new_name: '',
+        emailId: '',
+        new_institute: '',
+        old_pass: '',
+        new_pass: '',
+        touched: {
+            user: false,
+            ins: false,
+            pass:false
+        },
+        isLoading_info:false,
+        isLoading_pass:false
     }
 
+    componentDidMount() {
+        let {user} = this.context;
+        this.setState({
+            emailId:user.emailId,
+            new_name:user.username,
+            new_institute:user.institute
+        })
+    }
+
+    validate(user,ins,pass) {
+        const err ={
+            user: '',
+            ins: '',
+            pass:''
+        }
+        if(this.state.touched.user && user.length<3)
+            err.user='usernmae must be atleat of lenght 3';
+        else if(this.state.touched.pass && user.length>8)
+            err.user='username must be atmax of lenght 8';
+        if(this.state.touched.ins && ins.length===0)
+            err.ins='Addess must be non-empty';
+        if(this.state.touched.pass && pass.length<6)
+            err.pass='Password must be atleat of lenght 6';
+        return err;
+    }
+    
     //for touchng th box
     handleTouch =(feild) =>(e) => {
         this.setState({
@@ -35,85 +63,143 @@ class Edit extends Component {
         });
     }
 
-    //validating the form
-    validate(user,email,adds) {
-        const err ={
-            user: '',
-            email:'',
-            adds: ''
-        }
-        let mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    updateUserInfo=(e) => {
+        this.setState({
+            isLoading_info:true
+        })
+        e.preventDefault();
+        let {user}= this.context;
 
-        if(this.state.touched.email && !mailformat.test(email))
-            err.email='Mail format is not correct';
-        if(this.state.touched.user && user.length<3)
-            err.user='usernmae must be atleat of lenght 3';
-        else if(this.state.touched.pass && user.length>8)
-            err.user='username must be atmax of lenght 8';
-        if(this.state.touched.adds && adds.length===0)
-            err.adds='Addess must be non-empty';
-        return err;
+        let data={};
+        if(this.state.new_name!==user.username)
+            data.username=this.state.new_name;
+        if(this.state.new_institute!==user.institute)
+                data.institute=this.state.new_institute;
+        console.log(data);
+
+        axios.patch(baseUrl+`/user/edit/${user._id}`, data, {
+            headers: {
+                'Authorization': 'basic '+this.context.jwtToken
+            }
+        })
+        .then(res => {
+            this.setState({
+                isLoading_info:false
+            })
+            alert(res.data.status);
+            this.context.modifyInfo(res.data.user);
+        })
+        .catch(err => {
+            this.setState({
+                isLoading_info:false
+            })
+            console.log(err.response);
+            alert(err.response.data);
+        });
+    }
+
+    resetPassword=(e) => {
+        e.preventDefault();
+        this.setState({
+            isLoading_pass:true
+        })
+
+        let data={
+            old:this.state.old_pass,
+            new:this.state.new_pass
+        }
+
+        axios.patch(baseUrl+`/user/reset_password/${this.context.user._id}`, data, {
+            headers: {
+                'Authorization': 'basic '+this.context.jwtToken
+            }
+        })
+        .then(res => {
+            this.setState({
+                old_pass:'',
+                new_pass:'',
+                isLoading_pass:false
+            })
+            alert(res.data);
+        })
+        .catch(err => {
+            this.setState({
+                old_pass:'',
+                new_pass:'',
+                isLoading_pass:false
+            })
+            console.log(err.response);
+            alert(err.response.data);
+        });
     }
     
-
     render() {
-        let errs=this.validate(this.state.new_name, this.state.new_email,this.state.new_adds);
+        let errs=this.validate(this.state.new_name,this.state.new_institute,this.state.new_pass);
+
         return (
-            <div className="container user">
-                <div className="row">
-                    <div className="col-12">
-                        <Card>
-                        <CardHeader style={{color:'blue'}}>
-                                    <strong>Edit profile</strong>
-                            </CardHeader>
-                        </Card>
-                        <CardBody>
-                                <div className="row">
+            <div className="container mt-5 pt-1 user">
+                <div className="row justify-content-center">
+                    <div className="col-10 col-md-9">
+                        <div className="card">
+                            <div className="card-header border-0 bg-info text-white">
+                                <div className="row align-items-center">
+                                    <h4 className="col-auto ms-2">Edit profile</h4>
+
+                                    <div className="col-auto ms-auto me-3">
+                                        <Link to={`/user`}>
+                                            <button className="btn btn-outline-light">Back</button>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card-body">
+                                <div className="row mt-2">
                                     <div className="col-auto">
                                         <strong>Edit Basic Info.</strong>
                                         <hr></hr>
                                     </div> 
                                 </div>
 
-                                <div className="row p-5">
+                                <div className="row p-md-2">
                                     <div className="col-12 col-md-7">
-                                        <Form >
-                                            <FormGroup row>
-                                                <Label htmlFor="new_username" className="col-5">Username</Label>
-                                                <Input type="text" id="new_username" className="col-7"
-                                                    valid={errs.user===''}
-                                                    invalid={errs.user!==''}
-                                                    value={this.state.new_name}
-                                                    onBlur={this.handleTouch('user')}
-                                                    onChange={this.handleChange}/>
-                                            </FormGroup>
 
-                                            <FormGroup row>
-                                                <Label htmlFor="new_email" className="col-5">Email</Label>
-                                                <Input type="email" id="new_email" className="col-7"
-                                                    valid={errs.email===''}
-                                                    invalid={errs.email!==''}
-                                                    value={this.state.new_email}
-                                                    onBlur={this.handleTouch('email')}
-                                                    onChange={this.handleChange}/>
-                                            </FormGroup>
-
-                                            <FormGroup row>
-                                                <Label htmlFor="cnew_add" className="col-5">Address</Label>
-                                                <Input type="text" id="cnew_add" className="col-7"
-                                                    valid={errs.adds===''}
-                                                    invalid={errs.adds!==''}
-                                                    value={this.state.new_adds}
-                                                    onBlur={this.handleTouch('adds')}
-                                                    onChange={this.handleChange}/>
-                                            </FormGroup>
-
-                                            <FormGroup row>
-                                                <div className="col-6 ml-auto">
-                                                    <Button type="submit" color="primary" outline>Edit Info.</Button>
+                                        {this.state.isLoading_info
+                                        ?
+                                            <LoadingSpinner/>
+                                        :
+                                            <form onSubmit={this.updateUserInfo}>
+                                                <div className="row mb-3 px-3">
+                                                    <label htmlFor="new_email" className="form-label col-12 col-sm-6">Email</label>
+                                                    <Input type="email" className="col-12 col-md-7" readOnly value={this.state.emailId} valid={true}/>
                                                 </div>
-                                                </FormGroup>
-                                        </Form>
+                                                
+                                                <div className="row mb-3 px-3">
+                                                    <label htmlFor="new_username" className="form-label col-12 col-sm-6">Username</label>
+                                                    <Input type="text" id="new_name" className="col-12 col-md-7"
+                                                        valid={errs.user===''}
+                                                        invalid={errs.user!==''}
+                                                        value={this.state.new_name}
+                                                        onBlur={this.handleTouch('user')}
+                                                        onChange={this.handleChange}/>
+                                                </div>
+
+                                                <div className="row mb-3 px-3">
+                                                    <label htmlFor="cnew_add" className="form-label col-12 col-sm-6">Institute</label>
+                                                    <Input type="text" id="new_institute" className="col-12 col-md-7"
+                                                        valid={errs.ins===''}
+                                                        invalid={errs.ins!==''}
+                                                        value={this.state.new_institute}
+                                                        onBlur={this.handleTouch('ins')}
+                                                        onChange={this.handleChange}/>
+                                                </div>
+
+                                                <div className="row mb-5 px-3 justify-content-center">
+                                                    <div className="col-12 col-md-6">
+                                                        <button className="btn btn-outline-primary" type="submit">Edit Info.</button>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        }
                                     </div>
                                 </div>
 
@@ -124,32 +210,41 @@ class Edit extends Component {
                                     </div> 
                                 </div>
 
-                                <div className="row p-5">
+                                <div className="row p-md-2">
                                     <div className="col-12 col-md-7">
-                                        <Form >
-                                            <FormGroup row>
-                                                <Label htmlFor="old_pass" className="col-5">Old Password</Label>
-                                                <Input type="password" id="old_pass" className="col-7"
-                                                    value={this.state.old_pass}
-                                                    onChange={this.handleChange}/>
-                                            </FormGroup>
-
-                                            <FormGroup row>
-                                                <Label htmlFor="new_pass" className="col-5">New Password</Label>
-                                                <Input type="password" id="new_pass" className="col-7"
-                                                    value={this.state.new_pass}
-                                                    onChange={this.handleChange}/>
-                                            </FormGroup>
-
-                                            <FormGroup row>
-                                                <div className="col-6 ml-auto">
-                                                    <Button type="submit" color="primary" outline>Reset Password</Button>
+                                        {this.state.isLoading_pass
+                                        ?
+                                            <LoadingSpinner/>
+                                        :
+                                            <form onSubmit={this.resetPassword}>
+                                                <div className="row mb-3 px-3">
+                                                    <label htmlFor="old_pass" className="form-label col-12 col-sm-8">Old Password</label>
+                                                    <Input type="password" id="old_pass" className="col-12 col-md-7"
+                                                        value={this.state.old_pass}
+                                                        valid={true}
+                                                        onChange={this.handleChange}/>
                                                 </div>
-                                                </FormGroup>
-                                        </Form>
+
+                                                <div className="row mb-3 px-3">
+                                                    <label htmlFor="new_pass" className="form-label col-12 col-sm-8">New Password</label>
+                                                    <Input type="password" id="new_pass" className="col-12 col-md-6"
+                                                        value={this.state.new_pass}
+                                                        valid={errs.pass===''}
+                                                        invalid={errs.pass!==''}
+                                                        onChange={this.handleChange}/>
+                                                </div>
+
+                                                <div className="row mb-4 px-3 justify-content-center">
+                                                    <div className="col-12 col-sm-8">
+                                                        <button className="btn btn-outline-primary" type="submit">Reset Password</button>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        }
                                     </div>
                                 </div>
-                        </CardBody>
+                            </div>
+                        </div>
                     </div> 
                 </div>    
             </div>
--- frontend/src/components/User/profile.js
+++ frontend/src/components/User/profile.js
@@ -1,80 +1,72 @@
 import React, { Component } from 'react';
-import {
-    Card, CardImg, CardBody, Button, CardHeader} from 'reactstrap';
-import './style.css'
 import dummyUser from '../../assets/dummyUser.png'
 import { Link } from 'react-router-dom';
+import {authContext} from '../../contexts/authContext';
+import './style.css'
 
 class Profile extends Component {
-    constructor(props) {
-        super(props);
-        
-        this.state= {
-            username: 'Admin',
-            email: 'admin@gmail.com',
-            address: 'IIT Bhubaneswar'
-        }
-    }
+    static contextType=authContext;
 
-    
     render() {
+
+        const {user}=this.context;
+
         return (
-            <div className="container user">
-                <div className="row">
-                    <div className="col-12">
-                        <Card>
-                            <CardHeader style={{color:'blue'}}>
-                                <div className="row">
-                                    <strong>Profile Info.</strong>
-                                    <div className="ml-auto">
+            <div className="container user pt-5 px-2">
+                <div className="row justify-content-center">
+                    <div className="col-12 col-md-9">
+                        <div className="card">
+                            <div className="card-header border-0 bg-info text-white">
+                                <div className="row align-items-center">
+                                    <h4 className="col-auto ms-2">Profile Info.</h4>
+                                    
+                                    <div className="col-auto ms-auto me-3">
                                         <Link to={`/user/edit`}>
-                                            <Button className="fa fa-pencil fa-lg" outline onClick={this.toggleModal}></Button>
+                                            <button className="btn btn-outline-light"><span className="fa fa-pencil fa-lg"></span> </button>
                                         </Link>
                                     </div>
                                 </div>
-                            </CardHeader>
-                            <div className="row">
-                                <div className="col-8">
-                                    <CardBody>
-                                        <dl className="row p-1">
-                                            <dt className="col-5">User</dt>
-                                                <dd className="col-7">{this.state.username}</dd>
-                                            <dt className="col-5">E-mail</dt>
-                                                <dd className="col-7">{this.state.email}</dd>
-                                            <dt className="col-5">Address</dt>
-                                                <dd className="col-7">{this.state.address}</dd>
-                                        </dl>
-                                    </CardBody>
+                            </div>
+                            <div className="row align-items-center card-body">
+                                <div className="col-12 col-md-8 info">
+                                    <dl className="row">
+                                        <dt className="col-5">User</dt>
+                                            <dd className="col-7">{user.username}</dd>
+                                        <dt className="col-5">E-mail</dt>
+                                            <dd className="col-7">{user.emailId}</dd>
+                                        <dt className="col-5">Institute</dt>
+                                            <dd className="col-7">{user.institute}</dd>
+                                    </dl>
                                 </div>
-                                <div className="col-4 Img">
-                                    <CardImg className="img" src={dummyUser} alt="User" />
+                                <div className="col-4 d-none d-md-inline">
+                                    <img className="img-fluid" src={dummyUser} alt="User" />
                                 </div>
                             </div>
-                        </Card>
+                        </div>
                     </div>
                 </div>
 
-                <div className="row mt-3">
-                    <div className="col-12">
-                        <Card>
-                            <CardHeader style={{color:'blue'}}>
-                                    <strong>Stats</strong>
-                            </CardHeader>
-                            <div className="row">
+                <div className="row mt-5 justify-content-center">
+                    <div className="col-12 col-md-9">
+                        <div className="card">
+                            <div className="card-header border-0 bg-info text-white">
+                                    <h4>Stats</h4>
+                            </div>
+                            <div className="row align-items-center">
                                 <div className="col-10">
-                                    <CardBody>
+                                    <div className="card-body">
                                         <dl className="row p-1">
                                             <dt className="col-6">Total Created Quizzes</dt>
-                                                <dd className="col-6">{this.state.username}</dd>
+                                                <dd className="col-6">{user.createdList.length}</dd>
                                             <dt className="col-6">Total Attemted Quizzes</dt>
-                                                <dd className="col-6">{this.state.email}</dd>
+                                                <dd className="col-6">{user.takenList.length}</dd>
                                             <dt className="col-6">Best Performance</dt>
-                                                <dd className="col-6">{this.state.address}</dd>
+                                                <dd className="col-6">0</dd>
                                         </dl>
-                                    </CardBody>
+                                    </div>
                                 </div>
                             </div>
-                        </Card>
+                        </div>
                     </div>
                 </div>
             </div>
