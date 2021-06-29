45ded847aa04c84adf33ebf1f3ded516734a7566
yogesh-kansal <yogeshiitbhub@gmail.com>
2021-06-30 02:16:58 +0530

    modified test attempt

--- frontend/src/components/Dashboard/Test/attemptTest.js
+++ frontend/src/components/Dashboard/Test/attemptTest.js
@@ -6,94 +6,153 @@ import './test_style.css';
 
 class TakeTest extends Component {
     static contextType=authContext;
-    state = {
-        isStarted: false,
-        time :'2',
-        author:'admin',
-        questions:[{statement:'who are you? and where are you from?',options:['akash','yogesh','ram','shyam']},
-        {statement:'who are you?',options:['akash','yogesh','ram','shyam']},
-        {statement:'who are you?',options:['akash','yogesh','ram','shyam']}]
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isStarted: false,
+            test:[],
+            index:0
+            
+        }
+        
+    this.isTimeup=this.isTimeup.bind(this);
+        
     }
 
     componentDidMount() {
+         let answers=[];
+        for(let ques of this.context.data.questions)
+            answers.push(-1);
+        
         this.setState({
-            questions:this.context.data.questions
+            test:{...this.context.data,answers}
         })
     }
 
+    changeIndex(x) {
+        this.setState({
+            index:this.state.index+x
+        })
+    }
+
+    changeAns(index,id) {
+        console.log(index,id);
+        let {test}=this.state;
+        let answers=[...test.answers];
+        answers[index]=id;
+        test.answers=answers;
+
+        this.setState({
+            test
+        });
+    }
+
+    handleSubmit=(e) => {
+        if(e)
+            e.preventDefault();
+        console.log(this.state);
+        alert('Exam submiited successfully!!!');
+    }
+
     isTimeup() {
-        alert("time ot end the quiz");
+        alert('Time is up\nTest will submit automatically!!!');
+        this.handleSubmit();
     }
     
     render() {
-        let {isStarted,time,author} =this.state;
+        let {isStarted,test,index} =this.state;
+        console.log(isStarted, index)
         if(!isStarted)
             return(
                 <div className="container before">
                     <div className="row text-center justify-content-center">
-                        <div className="col-6">
-                            <p>Welcome to your new quiz<br/></p>
-                            <p>You are going to attempt the quiz<br/>
-                             which is {time} hours of time duration<br/>
-                             And created by {author}.</p>
-
-                            <Button color='primary' outline 
-                                onClick={()=>{this.setState({isStarted:true})}}
-                              >Start</Button>
+                        <div className="col-10 col-md-6">
+                            <p>You are going to attempt the test!!!</p>
+                            <span className="d-block">Duration: {test.duration} hour</span>
+                            <span className="d-block">Marks: {test.total}</span>
+                            <span className="d-block">Wr. Email: {test.author}</span>
+                            <div className="row mt-3 mx-5">
+                                <button type="button" className="btn btn-block btn-primary"
+                                    onClick={()=>{this.setState({isStarted:true})}}>START
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
             );
         else 
             return(
-                <div className="container after">
-                    <div className="row text-center">
-                        <div className="col-auto ml-auto mr-auto">
-                            <p>Quiz has been started!!!<br></br>
-                            You can end the quiz before time also.</p>
-                            <hr/>
-                        </div>
-                    </div>
-                    <div className="row">
+                <div className="container-fluid after">
+                    <div className="row mx-1">
                         <div className="col-12">
-                            <Form>
-                                <div className="row mb-5">
-                                    <div className="col-auto">
-                                        <Button type="submit" className="submit">Submit Quiz</Button>
-                                    </div>
-                                    <div className="col-auto ml-auto mr-5">
-                                        {<Timer timeUp={this.isTimeup}/>}
-                                    </div>
+                            <form onSubmit={this.handleSubmit}>
+                                <div className="row test-nav">
+                                        <div className="col-auto text-white">{test.name}</div>
+                                        <div className="col-auto ms-auto">
+                                            {<Timer time={test.duration} timeUp={this.isTimeup}/>}
+                                        </div>
+                                        <div className="ms-2 me-3 col-auto">
+                                            <button type="submit" className="btn btn-danger fs-5 submit">
+                                                <img className="me-1 mb-1" src={'https://files.codingninjas.in/0000000000000693.png'}></img>Submit</button>
+                                        </div>                            
                                 </div>
 
-                                <div className="row justify-content-center">
-                                    {this.state.questions.map((ques,index) => {
-                                        return(
-                                            <div key={index} className="col-10">
-                                                <div className="row mb-2">
-                                                    <div className="col-auto">
-                                                    {`Q ${index+1}.`} {ques.statement}
-                                                    </div>
-                                                </div>
-
-                                                <div className="row mb-5 pt-2 pl-5">                                                
-                                                    {ques.options.map((option,id) => {
-                                                        return(
-                                                            <div key={id} className="col-12 col-md-5 ml-auto mr-auto mb-2">
-                                                                <Label check><Input type="radio" className="mt-3" name="radio1"/>{' '}
-                                                                    {option}</Label>
-                                                            </div>
-                                                        )})} 
-                                                </div>
+                                <div className="row">
+                                    <div className="col-4 left-panel px-2">
+                                        <div className="row ms-1">
+                                            <div className="col-auto">
+                                                Question {index+1}.<hr></hr>
                                             </div>
-                                        )})}
+                                        </div>
+                                        <div className="row mx-1">
+                                            <div className="col-auto question">
+                                                {test.questions[index].statement} 
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-8 right-panel px-4">
+                                        <div className="row options">
+                                            <div className="col-auto">
+                                                Options<hr></hr>
+                                            </div>
+                                        </div>
+
+                                        <div className="row mb-5 pt-2 px-5 options flow py-3">                                                
+                                            {test.questions[index].options.map((option,id) => {
+                                                return(
+                                                    <div key={id} className="col-12 mb-2 form-check">
+                                                        <input className="form-check-input" type="radio" 
+                                                            checked={test.answers[index]===id}
+                                                            onChange={(e) =>this.changeAns(index,id)}>
+                                                        </input>
+                                                        <label className="form-check-label" for="">
+                                                            {option}
+                                                        </label>
+                                                    </div>
+                                                )})} 
+                                        </div>
+                                    </div>
                                 </div>
-                            </Form>
+
+                                <div className="row justify-content-center test-bottom">
+                                        <div className="me-1 col-auto">
+                                            <button type="button" className="btn text-white prev" disabled={index==0} onClick={() =>this.changeIndex(-1)}>
+                                                <img className="me-1 mb-1" src={'https://files.codingninjas.in/0000000000000690.png'}></img>Previous</button>
+                                        </div> 
+                                        <div className="ms-1 col-auto">
+                                            <button type="button" className="btn text-white next" disabled={test.questions.length===index+1} onClick={() =>this.changeIndex(1)}>
+                                                <img className="me-1 mb-1" src={'https://files.codingninjas.in/0000000000000689.png'}></img>Next</button>
+                                        </div>                            
+                                </div>
+
+                                
+                            </form>
                         </div>
                     </div>
                 </div>
             );   
     }
 }
-
+console.log(Component)
 export default TakeTest;
