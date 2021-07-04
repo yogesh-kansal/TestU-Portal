4006b1d996769d8fbb9943d4017ec5da2bad6513
yogesh-kansal <yogeshiitbhub@gmail.com>
2021-07-05 02:02:15 +0530

    mails

--- backend/utils/send_mail.js
+++ backend/utils/send_mail.js
@@ -1,36 +1,72 @@
-const catchAsync = require('./catchAsync');
 const config = require('./config');
 const nodemailer = require("nodemailer");
 
-exports.sendMail_to_verify= catchAsync(async (username, email, token) => {
+exports.setup=async () => {
+    let testAccount = await nodemailer.createTestAccount();
 
-    let info = await config.transporter.sendMail({
-        from: config.acc_user,
+  // create reusable transporter object using the default SMTP transport
+  config.transporter= nodemailer.createTransport({
+    host: testAccount.smtp.host,
+    port: testAccount.smtp.port,
+    secure: testAccount.smtp.secure,
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass // generated ethereal password
+    },
+    tls: {
+        rejectUnauthorized: false}
+  });
+
+  return{testAccount,transporter};
+};
+
+
+//verification mail
+
+exports.sendMail_to_verify= async (username, email, token) => {
+    let props=await setup();
+    let info = await props.transporter.sendMail({
+        from: props.acc_user,
         to: email, 
         subject: "Account verification",
         html: `<h2>Hello ${username}</h2>
                 <p>please verify your account by just clicking bwlow url</p>
-                <a href="http://localhost:3000/user/verifyUser?token=${token}">Click Here</a>
+                <a href="${config.baseUrl}/user/verifyUser?token=${token}">Click Here</a>
                 <br>
                 <p>With regards</p>
                 <p>Web Team</p>`,
       });
-
-      //console.log(info);
-    
       console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-});
+}
 
-exports.sendMail_to_change_pass= catchAsync(async (email, token) => {
-
-    let info = await config.transporter.sendMail({
-        from: config.acc_user,
+exports.sendMail_to_change_pass=async (email, token) => {
+    let props=await setup();
+    let info = await props.transporter.sendMail({
+        from: props.acc_user,
         to: email, 
         subject: "Change password",
         html: `<h2>Hello User</h2>
-                <p><a href="http://localhost:3000/user/redirects?token=${token}">Click Here</a> to change your password!!!</p>
+                <p><a href="${config.clientUrl}/forgotPswd?token=${token}">Click Here</a> to change your password!!!</p>
+                <br/>
                 <p>With regards</p>
                 <p> Web Team</p>`,
       });
       console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-});
+}
+
+exports.sendMail_to_users=async (email,test) => {
+    let props=await setup();
+    let info = await props.transporter.sendMail({
+        from: props.acc_user,
+        to: email, 
+        subject: "Change password",
+        html: `<h2>Hello User</h2>
+                <p>You are invited to new test ${test.name}. created by ${test.author}</p>
+                <p>Test is of  ${test.duration} hours with deadline to ${test.deadline}</p>
+                <P>You can attempt the test by visiting your profile in website</p>
+                <br/>
+                <p>With regards</p>
+                <p> Web Team</p>`,
+      });
+      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+}
