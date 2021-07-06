e07fcfee5f2e0e1dc4771a986023422ddbd4e985
yogesh-kansal <yogeshiitbhub@gmail.com>
2021-07-07 03:25:14 +0530

    modified

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var router = express.Router();
-const authcontroller = require('../controllers/authController');
+const express = require('express');
+const router = express.Router();
+const authcontroller = require('../controllers/authcontroller');
 const usercontroller = require('../controllers/usercontroller');
 const auth=require('../utils/verifyToken');
 
@@ -13,13 +13,13 @@ router.get('/logout', auth.verifytoken, authcontroller.logout);
 router.get('/verifyUser', authcontroller.verifyUser);
 
 //passwords
-router.patch('/reset_password/:id', auth.verifytoken, usercontroller.resetPassword);
 router.get('/forgot_password', authcontroller.forgotPassword);
-router.get('/redirects',usercontroller.redirects);
+router.patch('/reset_password/:id', auth.verifytoken, usercontroller.resetPassword);
 router.patch('/change_password', usercontroller.changePassword);
 
 router.patch('/edit/:id', auth.verifytoken, usercontroller.updateUser);
-router.get('/:id', auth.verifytoken, usercontroller.getUser);
+router.get('/', auth.verifytoken, usercontroller.getUser);
 router.delete('/:id', auth.verifytoken, usercontroller.deleteUser);
+
 
 module.exports = router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import { Component } from 'react';
+import AuthContextProvider from './contexts/authContext';
+import Routes from './routes';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'font-awesome/css/font-awesome.min.css'
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AuthContextProvider>
+          <Routes/>
+        </AuthContextProvider>
+      </div>
+    );
+
+  }
 }
 
 export default App;
