7e82468d57256fa301aac3686db7163c201aa78c
yogesh-kansal <yogeshiitbhub@gmail.com>
2021-07-07 03:25:41 +0530

    configs

---
+++ backend/utils/config.js
@@ -0,0 +1,10 @@
+module.exports={
+    secretKey: '12344321',
+    refreshKey:'1234567789',
+    genKEy:'121212121211',
+    dburl: 'mongodb://localhost:27017/project2',
+    baseUrl: 'http://localhost:3001',
+    clientUrl: 'http://localhost:3000'
+};
+
+//C:/Program Files/MongoDB/Server/4.4/bin/mongod.exe 
